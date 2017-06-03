36003d2a8db04a9c064fe75b6cf8b0275a812f7d
mrdj204 <mrdj204@users.noreply.github.com>
2017-06-03 15:25:46 -0400

    replaceMiddle

---
+++ arrays/replaceMiddle.java
@@ -0,0 +1,48 @@
+/*We define the middle of the array arr as follows:
+-if arr contains an odd number of elements, its middle is the element whose index number is the same when counting from the beginning of the array and from its end;
+-if arr contains an even number of elements, its middle is the sum of the two elements whose index numbers when counting from the beginning and from the end of the array differ by one.
+Given array arr, your task is to find its middle, and, if it consists of two elements, replace those elements with the value of middle. Return the resulting array as the answer.*/
+import java.util.Arrays;
+public class replaceMiddle {
+	
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{7,2,2,5,10,7}))); //expected output = [7, 2, 7, 10, 7]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{-5, -5, 10}))); //expected output = [-5, -5, 10]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{45, 23, 12, 33, 12, 453, -234, -45}))); //expected output = [45, 23, 12, 45, 453, -234, -45]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{2, 8}))); //expected output = [10]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{-12, 34, 40, -5, -12, 4, 0, 0, -12}))); //expected output = [-12, 34, 40, -5, -12, 4, 0, 0, -12]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{9, 0, 15, 9}))); //expected output = [9, 15, 9]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{-6, 6, -6}))); //expected output = [-6, 6, -6]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{26, 26, -17}))); //expected output = [26, 26, -17]
+		System.out.println(Arrays.toString(new replaceMiddle().replace(new int[]{-7, 5, 5, 10}))); //expected output = [-7, 10, 10]
+	}
+
+	int[] replace(int[] arr) {
+	    
+	    int len = arr.length;
+	    int j = 0;
+	    int mid = arr[len/2] + arr[(len/2)-1];
+	    int[] arr2 = new int[len-1];
+	    
+	    if (len%2 == 1)
+	        return arr;
+	    
+	    if (len == 2) {
+	        int[] arr3 = {arr[0] + arr[1]};
+	        return arr3;
+	    }
+	    
+	   for (int i=0; i<len; i++)
+	        if (i==len/2 || i==(len/2)-1) {
+	            arr2[arr2.length/2] = mid;
+	            j = (len/2);
+	        }else {
+	            arr2[j] = arr[i];
+	            j++;
+	        }
+	    
+	    return arr2;
+	    
+	}
+
+}
--- loops/candles.java
+++ loops/candles.java
@@ -1,3 +1,6 @@
+/*When a candle finishes burning it leaves a leftover. [makeNew] leftovers can be combined to make a new candle, which, when burning down, will in turn leave another leftover.
+You have [candlesNumber] candles in your possession. What's the total number of candles you can burn, assuming that you create new candles as soon as you have enough leftovers?*/
+
 public class candles {
 	
 	public static void main(String[] args) {
@@ -6,9 +9,6 @@ public class candles {
 		System.out.println(new candles().count(3,3)); //expected output = 4
 		System.out.println(new candles().count(11,3)); //expected output = 16
 	}
-
-	/*When a candle finishes burning it leaves a leftover. [makeNew] leftovers can be combined to make a new candle, which, when burning down, will in turn leave another leftover.
-	You have [candlesNumber] candles in your possession. What's the total number of candles you can burn, assuming that you create new candles as soon as you have enough leftovers?*/
 
 	int count(int candlesNumber, int makeNew) {
 	    int burned = 0, leftover = 0;
